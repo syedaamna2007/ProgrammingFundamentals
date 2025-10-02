b8303e7b807549c6ac3d88b1b3804ea7c6cd3880
Syeda Amna Hasnain <syedaamna2007@gmail.com>
2025-10-03 02:37:57 +0500

    Update LAB_6_Q2.c

--- Labs/Lab_6/LAB_6_Q2.c
+++ Labs/Lab_6/LAB_6_Q2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
-    int i, j;
+    int i, j, r;
+    for (int r = 1; r <= 2; r++) 
     for (i = 1; i <= 5; i++) {
         for (j = 1; j <= i; j++) {
             printf("*");
