6b31275394a9a02a1cb1645d9d95f1edfddf9de5
Syeda Amna Hasnain <syedaamna2007@gmail.com>
2025-10-20 20:32:15 +0500

    Update Lab_6_Q1.c

--- Lab_Exercises/Lab_6/Lab_6_Q1.c
+++ Lab_Exercises/Lab_6/Lab_6_Q1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
-	int n, fact=1;
+	int n;
+	long long int fact=1;
 	printf("Enter a number: ");
 	scanf("%d",&n);
 	int t=n;
@@ -8,12 +9,19 @@ int main(){
 	if (n<0){
 		return 0;
 	}
+	else if (n==0 || n==1){
+		printf("The factorial of %d is 1.\n", n);
+		printf("%d!=1",n);}
+	else if (n>20){
+		printf("Error: Value is too large to be stored.\nplease select a number from 0 to 20");
+	}
 	else {
 		while(t>=2){
 			fact *= t;
 			t=t-1;
 		}
-		printf("Factorial of %d = %d", n, fact);
+		printf("Factorial of %d is %llu.\n", n, fact);
+		printf("%d!=%llu",n,fact);
 		return 0;
 	}
 }
