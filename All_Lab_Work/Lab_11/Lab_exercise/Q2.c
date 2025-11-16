1c97a7e369d4e1a6248a5937da2af0355112324c
Syeda Amna Hasnain <syedaamna2007@gmail.com>
2025-11-16 19:40:56 +0500

    Create Q2.c

---
+++ All_Lab_Work/Lab_11/Lab_exercise/Q2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+char myFunction(){
+	float m, e;
+	printf("Enter your test marks: ");
+	scanf("%f", &m);
+	printf("Enter your years of experience: ");
+	scanf("%f", &e);
+	
+	if (m>=70 && e>=2){
+		return 'a';
+	}
+	else if (m>=60 && e>=1){
+		return 'b';
+	}
+	else if (m>=50){
+		return 'c';
+	}
+	else {
+		return 'd';
+	}
+}
+int main (){
+	char yourPosition = myFunction();
+	if (yourPosition=='a'){
+		printf("ASSIGNED POSITION: Associate Developer");
+	}
+	else if (yourPosition=='b'){
+		printf("ASSIGNED POSITION: Assistant Developer");
+	}
+	else if (yourPosition=='c'){
+		printf("ASSIGNED POSITION: Trainee Engineer");
+	}
+	else if (yourPosition=='d'){
+		printf("ASSIGNED POSITION: NOT ELIGIBLE!!");
+	}
+	return 0;
+}
