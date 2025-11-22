4eb9cbc8ae95c8914b29969cca43df51b78fb1b7
Syeda Amna Hasnain <syedaamna2007@gmail.com>
2025-11-23 00:01:16 +0500

    LEETCODE

---
+++ LEETCODE/prob11_failed.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    int check [head]={0};
+    int n= head;
+    while(n>0){
+        if (check==0){
+        check== n;}
+        else {
+            n--;
+        }
+
+    }
+    return head;
+}
